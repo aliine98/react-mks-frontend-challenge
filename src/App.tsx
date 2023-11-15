1d674b6a3e9364dbe71cb8b59066e3e98c4b6aeb
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-11-14 21:11:24 -0300

    ✨ feat: finish products list style

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,25 @@ const queryClient = new QueryClient();
 
 const ProductsGrid = styled.ul`
     list-style: none;
+    padding: 0;
+    max-width: 600px;
     display: grid;
     gap: 1.5rem;
+    justify-items: center;
+    margin: 1.2rem auto;
+    @media screen and (min-width: 630px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media screen and (min-width: 1000px) {
+        grid-template-columns: repeat(3, 1fr);
+        max-width: 810px;
+    }
+
+    @media screen and (min-width: 1400px) {
+        grid-template-columns: repeat(4, 1fr);
+        max-width: 900px;
+    }
 `;
 
 function App() {
