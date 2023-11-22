af25673079142ed972b1a26ddcdd6b98b73c6a06
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-11-21 21:07:33 -0300

    ✨ feat: finish styling + add to cart logic

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 import { GetProducts } from '../api/products-service';
+import Checkout from './Checkout';
+import { createContext, useState } from 'react';
 import { productData } from '../api/ProductData';
+
+const CartButton = styled.button`
+    border-radius: 8px;
+    background: #fff;
+    border: none;
+    padding: 6px 8px;
+    font-family: 'Montserrat', sans-serif;
+    display: flex;
+    font-weight: 700;
+    align-items: center;
+    position: fixed;
+    top: 11px;
+    right: 1rem;
+    column-gap: 15px;
+    cursor: pointer;
+    z-index: 2;
+`;
 
 const Card = styled.li`
     padding: 10px 15px 0;
@@ -56,29 +75,60 @@ const BuyButton = styled.button`
     cursor: pointer;
 `;
 
+const ProductCtxDefaultValue = {
+    products: [],
+    setProducts: () => {},
+};
+
+export const ProductsContext = createContext<{
+    products: productData[] | [];
+    setProducts: React.Dispatch<React.SetStateAction<[] | productData[]>>;
+}>(ProductCtxDefaultValue);
+
 export default function ProductCards() {
+    const [selectedProducts, setSelectedProducts] = useState<productData[] | []>(ProductCtxDefaultValue.products);
+    const value = { selectedProducts, setSelectedProducts };
+    const [isOpen, setIsOpen] = useState(false);
+    const productsInCart = selectedProducts.length;
+
+    function openCheckoutMenu() {
+        setIsOpen(!isOpen);
+    }
+
     const fetchProducts = GetProducts();
     if (fetchProducts.isPending) return 'Loading...';
     if (fetchProducts.error) return 'Error';
 
     return (
         <>
-            {fetchProducts.data?.products.map((product: productData) => (
-                <Card key={product.id}>
-                    <ProductPhoto src={product.photo} alt='' width={160} height={150} />
-                    <Wrapper>
-                        <ProductTile>
-                            {product.brand} {product.name}
-                        </ProductTile>
-                        <PriceTag>R${Number(product.price)}</PriceTag>
-                    </Wrapper>
-                    <P>{product.description}</P>
-                    <BuyButton>
-                        <img src='src/assets/shopping-bag.svg' alt='shopping bag' />
-                        Comprar
-                    </BuyButton>
-                </Card>
-            ))}
+            <ProductsContext.Provider value={value}>
+                <CartButton onClick={openCheckoutMenu}>
+                    <img src='/src/assets/cart.svg' alt='cart' />
+                    {productsInCart}
+                </CartButton>
+                <Checkout opened={isOpen} setOpened={setIsOpen} />
+                {fetchProducts.data?.products.map((product: productData) => (
+                    <Card key={product.id}>
+                        <ProductPhoto src={product.photo} alt='' width={160} height={150} />
+                        <Wrapper>
+                            <ProductTile>
+                                {product.brand} {product.name}
+                            </ProductTile>
+                            <PriceTag>R${Number(product.price)}</PriceTag>
+                        </Wrapper>
+                        <P>{product.description}</P>
+                        <BuyButton
+                            onClick={() => {
+                                if (selectedProducts.includes(product)) return;
+                                product.quantity = 1;
+                                setSelectedProducts(selectedProducts.concat(product));
+                            }}>
+                            <img src='src/assets/shopping-bag.svg' alt='shopping bag' />
+                            Comprar
+                        </BuyButton>
+                    </Card>
+                ))}
+            </ProductsContext.Provider>
         </>
     );
 }
