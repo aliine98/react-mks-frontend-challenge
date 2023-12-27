be192b52b3e160260e6542e8d7aea95100987f3d
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2023-12-27 00:20:39 -0300

    ♻️ refactor: add totalPrice calc state

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import { useContext, useEffect, useState} from 'react';
 import { styled } from 'styled-components';
 import { productData } from '../api/ProductData';
-import { PriceTag } from './shared/styled-components';
+import { StoreContext } from './Store';
+import CartItem from './CartItem';
 
 const CheckoutMenu = styled.div`
     position: fixed;
@@ -53,91 +54,6 @@ const CartList = styled.ul`
     padding-top: 0.8rem;
 `;
 
-const CartProduct = styled.li`
-    background-color: #fff;
-    padding: 10px;
-    color: #2c2c2c;
-    width: 80%;
-    border-radius: 8px;
-    position: relative;
-
-    @media screen and (min-width: 500px) {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 90%;
-
-        img {
-            width: 70px;
-            height: 70px;
-        }
-    }
-`;
-
-const DeleteButton = styled.button`
-    background-color: #000;
-    color: #fff;
-    padding: 7px 10px;
-    border: none;
-    border-radius: 100%;
-    position: absolute;
-    top: -10px;
-    right: -10px;
-    cursor: pointer;
-`;
-
-const Title = styled.h5`
-    font-weight: 500;
-
-    @media screen and (min-width: 500px) {
-        width: 100px;
-    }
-`;
-
-const QuantityInput = styled.input`
-    border-top: 1px solid #bfbfbf;
-    border-bottom: 1px solid #bfbfbf;
-    border-right: none;
-    border-left: none;
-    width: 30px;
-    padding: 5px;
-    -webkit-appearance: textfield;
-    -moz-appearance: textfield;
-    appearance: textfield;
-
-    &::-webkit-inner-spin-button,
-    &::-webkit-outer-spin-button {
-        -webkit-appearance: none;
-    }
-
-    @media screen and (min-width: 500px) {
-        width: 25px;
-    }
-`;
-
-const QuantityButton = styled.button`
-    border-top: 1px solid #bfbfbf;
-    border-bottom: 1px solid #bfbfbf;
-    padding: 5px;
-    border-radius: 4px;
-    background: none;
-    width: 30px;
-    cursor: pointer;
-
-    &.minus {
-        border-left: 1px solid #bfbfbf;
-        border-right: none;
-    }
-    &.plus {
-        border-right: 1px solid #bfbfbf;
-        border-left: none;
-    }
-
-    @media screen and (min-width: 500px) {
-        width: 25px;
-    }
-`;
-
 const TotalPrice = styled.h3`
     margin-right: 2rem;
 `;
@@ -153,65 +69,24 @@ const CheckoutPurchaseButton = styled.button`
     width: 100%;
 `;
 
-type CheckoutProps = {
-    opened: boolean;
-    setOpened: React.Dispatch<React.SetStateAction<boolean>>;
-    selectedProducts: productData[];
-    setSelectedProducts: React.Dispatch<React.SetStateAction<productData[]>>;
-};
+export default function Checkout() {
+    const { state, dispatch } = useContext(StoreContext);
+    const [totalPrice,setTotalPrice] = useState(0);
 
-export default function Checkout({ opened, setOpened, selectedProducts, setSelectedProducts }: CheckoutProps) {
-    const [, setProductQuantity] = useState(1);
-    const totalPrice = selectedProducts?.reduce((acc: number, p: productData) => acc + p.price * p.quantity, 0);
+    useEffect(() => {
+        setTotalPrice(state.productsQty.reduce((acc:number,item:{id:number,price:number}) => acc + item?.price,0));
+    },[state.selectedProducts, state.productsQty,totalPrice]);
 
     return (
         <>
-            <CheckoutMenu className={opened ? 'open' : ''}>
+            <CheckoutMenu className={state.isOpen ? 'open' : ''}>
                 <Wrapper>
                     <CheckoutTitle>Carrinho de compras</CheckoutTitle>
-                    <CloseButton onClick={() => setOpened!(!opened)}>X</CloseButton>
+                    <CloseButton onClick={() => dispatch({ type: 'openCloseCheckout' })}>X</CloseButton>
                 </Wrapper>
                 <CartList>
-                    {selectedProducts?.map((product: productData, index: number) => (
-                        <CartProduct key={index}>
-                            <DeleteButton onClick={() => setSelectedProducts!(selectedProducts?.filter((p: productData) => p != product))}>
-                                X
-                            </DeleteButton>
-                            <img src={product.photo} width='90' height='90' alt={product.name} />
-                            <Title>
-                                {product.brand} {product.name}
-                            </Title>
-                            <Wrapper>
-                                <div>
-                                    <QuantityButton
-                                        className='minus'
-                                        onClick={() => {
-                                            product.quantity -= 1;
-                                            setProductQuantity(product.quantity - 1);
-                                        }}>
-                                        -
-                                    </QuantityButton>
-                                    <QuantityInput
-                                        type='number'
-                                        value={product.quantity}
-                                        onChange={e => {
-                                            setProductQuantity(Number(e.target.value));
-                                            product.quantity = Number(e.target.value);
-                                        }}
-                                        min='1'
-                                    />
-                                    <QuantityButton
-                                        className='plus'
-                                        onClick={() => {
-                                            product.quantity += 1;
-                                            setProductQuantity(product.quantity + 1);
-                                        }}>
-                                        +
-                                    </QuantityButton>
-                                </div>
-                                <PriceTag className='checkout-price'>R${Number(product.price) * product.quantity}</PriceTag>
-                            </Wrapper>
-                        </CartProduct>
+                    {state.selectedProducts?.map((product: productData) => (
+                        <CartItem key={product.id} product={product} />
                     ))}
                 </CartList>
                 <Wrapper>
